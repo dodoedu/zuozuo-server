0b7eac9775efab3308f9fd01e5249731b2abb970
Fernando Zhu <fernandoisadev@gmail.com>
2017-07-09 22:20:15 +1200

    Added basic database schema

---
+++ zuozuo/courses/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Course(models.Model):
+    """Courses definition"""
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    school = models.ForeignKey('schools.School', related_name="courses", null=True)
+
+    @classmethod
+    def create(cls, **kwargs):
+        """Class method responsible for creating and saving a Course object in database"""
+        course = cls(**kwargs)
+        course.save()
+        return course
+
+
+class Announcement(models.Model):
+    """Course assignments"""
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    course = models.ForeignKey(Course, related_name="announcements")
+
+
+class Test(models.Model):
+    """Course tests"""
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    course = models.ForeignKey(Course, related_name="tests")
+
+
+class Assignment(models.Model):
+    """Course assignments"""
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    course = models.ForeignKey(Course, related_name="assignments")
+
+
+class Resource(models.Model):
+    """Course resources, for example PPT"""
+    created_at = models.DateTimeField(auto_now_add=True)
+    course = models.ForeignKey(Course, related_name="resources")
--- zuozuo/zuozuo/settings.py
+++ zuozuo/zuozuo/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'courses',
+    'schools',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    )
+}
+
