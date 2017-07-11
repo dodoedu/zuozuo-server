76292078cca9c3757b06d7c3e045dd1d650c8498
Fernando Zhu <fernandoisadev@gmail.com>
2017-07-11 20:28:32 +1200

    Added CRUD APIs for course assignments, tests and resources

--- zuozuo/courses/urls.py
+++ zuozuo/courses/urls.py
@@ -3,11 +3,33 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ListCreateCourse.as_view(), name="course_list"),
-    url(r'(?P<pk>\d+)/$', views.RetrieveUpdateDestroyCourse.as_view(), name="course_detail"),
-    url(r'^(?P<course_pk>\d+)/ann/$', views.ListCreateAnnouncement.as_view(),
+    url(r'^$',
+        views.ListCreateCourse.as_view(),
+        name="course_list"),
+    url(r'(?P<pk>\d+)/$',
+        views.RetrieveUpdateDestroyCourse.as_view(),
+        name="course_detail"),
+    url(r'^(?P<course_pk>\d+)/ann/$',
+        views.ListCreateAnnouncement.as_view(),
         name="announcement_list"),
     url(r'^(?P<course_pk>\d+)/ann/(?P<pk>\d+)/$',
         views.RetrieveUpdateDestroyAnnouncement.as_view(),
-        name="announcement_detail")
+        name="announcement_detail"),
+    url(r'^(?P<course_pk>\d+)/test/$',
+        views.ListCreateTest.as_view(),
+        name="test_list"),
+    url(r'^(?P<course_pk>\d+)/test/(?P<pk>\d+)/$',
+        views.RetrieveUpdateDestroyTest.as_view(),
+        name="test_detail"),
+    url(r'^(?P<course_pk>\d+)/ass/$', views.ListCreateAssignment.as_view(),
+        name="assignment_list"),
+    url(r'^(?P<course_pk>\d+)/ass/(?P<pk>\d+)/$',
+        views.RetrieveUpdateDestroyAssignment.as_view(),
+        name="assignment_detail"),
+    url(r'^(?P<course_pk>\d+)/resource/$',
+        views.ListCreateResource.as_view(),
+        name="resource_list"),
+    url(r'^(?P<course_pk>\d+)/resource/(?P<pk>\d+)/$',
+        views.RetrieveUpdateDestroyResource.as_view(),
+        name="resource_detail")
 ]
--- zuozuo/courses/views.py
+++ zuozuo/courses/views.py
@@ -25,3 +25,42 @@ class RetrieveUpdateDestroyAnnouncement(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Announcement.objects.all()
     serializer_class = serializers.AnnouncementSerializer
 
+
+class ListCreateTest(generics.ListCreateAPIView):
+    queryset = models.Test.objects.all()
+    serializer_class = serializers.TestSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(course_id=self.kwargs.get('course_pk'))
+
+
+class RetrieveUpdateDestroyTest(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Test.objects.all()
+    serializer_class = serializers.TestSerializer
+
+
+class ListCreateAssignment(generics.ListCreateAPIView):
+    queryset = models.Assignment.objects.all()
+    serializer_class = serializers.AssignmentSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(course_id=self.kwargs.get('course_pk'))
+
+
+class RetrieveUpdateDestroyAssignment(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Assignment.objects.all()
+    serializer_class = serializers.AssignmentSerializer
+
+
+class ListCreateResource(generics.ListCreateAPIView):
+    queryset = models.Resource.objects.all()
+    serializer_class = serializers.ResourceSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(course_id=self.kwargs.get('course_pk'))
+
+
+class RetrieveUpdateDestroyResource(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Resource.objects.all()
+    serializer_class = serializers.ResourceSerializer
+
